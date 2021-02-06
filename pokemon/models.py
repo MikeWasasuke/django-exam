21583d3620a9a022898459dc0a2786c4f570aa87
Jorge Rojas <tercerohorheiii@gmail.com>
2021-02-06 22:21:09 +0800

    reformat code

--- pokemon/models.py
+++ pokemon/models.py
@@ -6,20 +6,19 @@ class PokemonSpecie(models.Model):
     pokemon_name = models.CharField(max_length=250, blank='False')
     pokemon_type = models.ManyToManyField('PokemonType', blank='False')
     evolution_level = models.IntegerField(null=True)
-    next_evolution = models.CharField(max_length=250, null=True)
+    next_evolution = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.pokemon_name
 
 
 class PokemonType(models.Model):
-    pokemon_type = models.CharField(max_length=150, null=True)
+    pokemon_type = models.CharField(max_length=150, blank='False')
 
     def __str__(self):
         return self.pokemon_type
 
 
-# foreign key https://stackoverflow.com/questions/14663523/foreign-key-django-model
 class Pokemon(models.Model):
     name = models.CharField(max_length=150, verbose_name="Pokemon", blank='False')
     species = models.ForeignKey('PokemonSpecie', blank='False', on_delete=models.CASCADE)
@@ -28,3 +27,6 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return self.name
+
+# foreign key https://stackoverflow.com/questions/14663523/foreign-key-django-model  
+# https://stackoverflow.com/questions/8609192/what-is-the-difference-between-null-true-and-blank-true-in-django
