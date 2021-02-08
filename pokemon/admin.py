991f0dd17c3c87e95a3f5cec72064cb62708a83c
Jorge Rojas <tercerohorheiii@gmail.com>
2021-02-08 20:58:00 +0800

    final

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -5,13 +5,20 @@ from .models import PokemonSpecie, PokemonType, Pokemon
 # Register your models here.
 @admin.register(PokemonSpecie)
 class PokemonSpecieAdmin(admin.ModelAdmin):
-    list_display = ('pokemon_name',
-                    'pokemon_type',
+    list_display = ('name',
                     'evolution_level',
-                    'next_evolution')
+                    'next_evolution',
+                    'show_types')
+
+    filter_horizontal=('pokemon_type', )
+
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    list_display = ('nickname',
+                    'species',
+                    'level',
+                    'trainer',
+                    )
 
 
-admin.site.register(Pokemon)
 admin.site.register(PokemonType)
-#admin.site.register(PokemonSpecie)
-
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,33 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-# https://books.agiliq.com/projects/django-orm-cookbook/en/latest/self_fk.html
+# https://stackoverflow.com/questions/15285626/django-self-referential-foreign-key
 class PokemonSpecie(models.Model):
-    pokemon_name = models.CharField(max_length=250, blank='False')
-    pokemon_type = models.ForeignKey('PokemonType', on_delete=models.CASCADE, null=True)
-    evolution_level = models.PositiveIntegerField(null=True)
-    next_evolution = models.CharField(max_length=200, null=True)
+    name = models.CharField(max_length=250)
+    pokemon_type = models.ManyToManyField('PokemonType', verbose_name='pokemon types')
+    evolution_level = models.PositiveIntegerField(blank=True, null=True)
+    next_evolution = models.ForeignKey('PokemonSpecie', blank=True, null=True, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.pokemon_name
-
-
-class PokemonType(models.Model):
-    pokemon_type = models.CharField(max_length=150, blank='False')
-
-    def __str__(self):
-        return self.pokemon_type
-
-
-class Pokemon(models.Model):
-    name = models.CharField(max_length=150, verbose_name="Pokemon", blank='False')
-    species = models.ForeignKey('PokemonSpecie', blank='False', on_delete=models.CASCADE)
-    level = models.PositiveIntegerField(null=True)
-    trainer = models.CharField(max_length=250, null=True)
+    def show_types(self):
+        return ", ".join([p.name for p in self.pokemon_type.all()])
 
     def __str__(self):
         return self.name
+
+
+class PokemonType(models.Model):
+    name = models.CharField(max_length=150, blank='False')
+
+    def __str__(self):
+        return self.name
+
+
+class Pokemon(models.Model):
+    nickname = models.CharField(max_length=150)
+    species = models.ForeignKey('PokemonSpecie', on_delete=models.CASCADE)
+    level = models.PositiveIntegerField()
+    trainer = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "Pokemon"
+        verbose_name_plural = 'Pokemon'
+
+    # https://stackoverflow.com/questions/50050862/django-override-model-save/50051005
+    def save(self, *args, **kwargs):
+        if self.level >= self.species.evolution_level:
+            self.species = self.species.next_evolution
+        super(Pokemon, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nickname
 
 # foreign key https://stackoverflow.com/questions/14663523/foreign-key-django-model
 # https://stackoverflow.com/questions/8609192/what-is-the-difference-between-null-true-and-blank-true-in-django
